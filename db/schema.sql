42b13f8c9e8aa24c9225e86ebb8fac4c785d9a19
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-08-09 08:53:48 -0700

    Initial File Commit

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+create database burgers_db;
+
+use burgers_db;
+
+create table burgers(
+    id int auto_increment primary key,
+    burger_name varchar(20),
+    devoured boolean,
+)
+
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+use burgers_db;
+
+insert into burgers (burger_name, devoured)
+values
+("Veggie", 0),
+("Bacon Cheesburger", 0),
+("Southwest Ranch", 0),
+("Classic", 0 ),
+("Avacado Toast", 0);
